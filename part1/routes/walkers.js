877c80fc42ad1de75d5385645368cf0750c57641
henryn3005 <ineverdie69@hotmail.com>
2025-06-21 03:34:00 +0000

    [automatic commit] 2025-06-21 03:34:00 UTC 14.2.84.218

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -5,9 +5,9 @@ var db = require('../db');
 
 router.get('/api/walers/summary', async (req, res) => {
     const [rows] = await db.query(`
-        SELECT d.name AS dog_name, d.size, u.username AS owner_username
-        FROM Dogs d
-        JOIN Users u ON d.owner_id = u.user_id
+        SELECT u.username AS walker_username,
+        COUNT(r.rating_id) AS total_ratings,
+        ROUND(AVG)
         `);
         res.json(rows);
 });
