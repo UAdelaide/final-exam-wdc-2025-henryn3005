8f94968a3200a06b938730ba4ea188fd98442b12
henryn3005 <ineverdie69@hotmail.com>
2025-06-21 03:27:39 +0000

    [automatic commit] 2025-06-21 03:27:39 UTC 14.2.84.218

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -6,7 +6,8 @@ var db = require('../db');
 router.get('api/dogs', async (req, res) => {
     const [rows] = await db.query(`
         SELECT d.name AS dog_name, d.size, u.username AS owner_username
-        FROM dog
+        FROM Dogs d
+        JOIN Users u ON d.owner_id = u.user_id
         `);
         res.json(rows);
 });
