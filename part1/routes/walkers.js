d508b9563dcf907a2ac749acc781fe18ac57395f
henryn3005 <ineverdie69@hotmail.com>
2025-06-21 03:49:25 +0000

    [automatic commit] 2025-06-21 03:49:25 UTC 14.2.84.218

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -5,7 +5,25 @@ var db = require('../db');
 
 router.get('/api/walers/summary', async (req, res) => {
     try {
-
+        const [rows] = await db.query(`
+        SELECT u.username AS walker_username,
+        COUNT(r.rating_id) AS total_ratings,
+        ROUND(AVG(r.rating),1) AS average_rating,
+        (
+        SELECT COUNT(*)
+        FROM WalkRequests wr
+        JOIN WalkApplications wa ON wr.request_id = wa.request_id
+        WHERE wr.status = 'completed' AND wa.walker_id = u.user_id
+        )
+        AS completed_walks
+        FROM User u
+        LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
+        WHERE u.role = 'walker'
+        GROUP BY u.user_id
+        `);
+        res.json(rows);
+    } catch (err) {
+        res.status(500).
     }
 
 });
