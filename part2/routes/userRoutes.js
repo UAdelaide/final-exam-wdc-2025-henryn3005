ba284aba89db3fbb0a0e79c7dcb900cf453c82ea
henryn3005 <ineverdie69@hotmail.com>
2025-06-21 06:04:46 +0000

    [automatic commit] 2025-06-21 06:04:46 UTC 14.2.84.218

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,9 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    
+    req.session.user = {
+      user_id: rows[0].user_id
+    }
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
