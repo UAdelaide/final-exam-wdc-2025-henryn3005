b7136ee891798831eb762b5f4f8f0f57738c643b
henryn3005 <ineverdie69@hotmail.com>
2025-06-21 03:38:02 +0000

    [automatic commit] 2025-06-21 03:38:02 UTC 14.2.84.218

--- part1/app.js
+++ part1/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dogRouter = require('./routes/dogs');
+var walkersRouter = require('./routes/dogs');
+var walkrequestsRouter = require('./routes/dogs');
 
 var app = express();
 
