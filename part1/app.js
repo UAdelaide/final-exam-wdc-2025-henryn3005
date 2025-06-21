d564ae1f087db6602b666881770df16e5009ba2f
henryn3005 <ineverdie69@hotmail.com>
2025-06-21 04:16:38 +0000

    [automatic commit] 2025-06-21 04:16:38 UTC 14.2.84.218

--- part1/app.js
+++ part1/app.js
@@ -111,7 +111,7 @@ let db;
       await db.execute(`
         INSERT INTO Users (username, email, password_hash, role) VALUES
         ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+        ('carol123', 'carol@example.com', 'hashed123', 'owner'),
         ('alice123', 'alice@example.com', 'hashed123', 'owner'),
         ('alice123', 'alice@example.com', 'hashed123', 'owner'),
         ('alice123', 'alice@example.com', 'hashed123', 'owner'),
