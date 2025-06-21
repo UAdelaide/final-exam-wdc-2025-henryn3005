c17259453c8dd848edc775dcf619089a7097b1a2
henryn3005 <ineverdie69@hotmail.com>
2025-06-21 03:30:20 +0000

    [automatic commit] 2025-06-21 03:30:20 UTC 14.2.84.218

---
+++ part1/routes/walkers.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../db');
+
+
+router.get('/api/dogs', async (req, res) => {
+    const [rows] = await db.query(`
+        SELECT d.name AS dog_name, d.size, u.username AS owner_username
+        FROM Dogs d
+        JOIN Users u ON d.owner_id = u.user_id
+        `);
+        res.json(rows);
+});
+
+module.exports = router;
