c83547a9dfd2ad0991c544326ddb0011c6f3ebe5
henryn3005 <ineverdie69@hotmail.com>
2025-06-21 03:33:00 +0000

    [automatic commit] 2025-06-21 03:33:00 UTC 14.2.84.218

--- part1/routes/walkrequests.js
+++ part1/routes/walkrequests.js
@@ -8,7 +8,8 @@ router.get('/api/walkrequests/open', async (req, res) => {
         SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
         FROM WalkRequests wr
         JOIN Dogs d ON wr.dog_id = d.dog_id
-        JOIN 
+        JOIN Users u ON d.owner_id = u.user_id
+        WHERE wr.status = 'open'
         `);
         res.json(rows);
 });
