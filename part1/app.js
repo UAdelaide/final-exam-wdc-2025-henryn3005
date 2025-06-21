ae63b38228af5671f8e2f4184c21f518bc89a297
henryn3005 <ineverdie69@hotmail.com>
2025-06-21 04:02:14 +0000

    [automatic commit] 2025-06-21 04:02:14 UTC 14.2.84.218

--- part1/app.js
+++ part1/app.js
@@ -54,8 +54,7 @@ let db;
     await db.execute(`
       CREATE TABLE IF NOT EXISTS Dogs (
         dog_id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(50) UNIQUE NOT NULL,
-        email VARCHAR(50) UNIQUE NOT NULL,
+        owner_id INT NOT NULL,
         password_hash VARCHAR(255) NOT NULL,
         role ENUM('owner','walker') NUT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
