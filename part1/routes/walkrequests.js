d8bbbdd774b7a62b08fcf253c02f094d418d8377
henryn3005 <ineverdie69@hotmail.com>
2025-06-21 03:48:05 +0000

    [automatic commit] 2025-06-21 03:48:05 UTC 14.2.84.218

--- part1/routes/walkrequests.js
+++ part1/routes/walkrequests.js
@@ -4,14 +4,10 @@ var db = require('../db');
 
 
 router.get('/api/walkrequests/open', async (req, res) => {
-    const [rows] = await db.query(`
-        SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
-        FROM WalkRequests wr
-        JOIN Dogs d ON wr.dog_id = d.dog_id
-        JOIN Users u ON d.owner_id = u.user_id
-        WHERE wr.status = 'open'
-        `);
-        res.json(rows);
+    try {
+
+    }
+
 });
 
 module.exports = router;
