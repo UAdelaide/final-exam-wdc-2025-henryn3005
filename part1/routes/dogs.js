129cc05107cdddaf35a83604c8b53b728e9619a4
henryn3005 <ineverdie69@hotmail.com>
2025-06-21 03:27:59 +0000

    [automatic commit] 2025-06-21 03:27:59 UTC 14.2.84.218

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var db = require('../db');
 
 
-router.get('api/dogs', async (req, res) => {
+router.get('/api/dogs', async (req, res) => {
     const [rows] = await db.query(`
         SELECT d.name AS dog_name, d.size, u.username AS owner_username
         FROM Dogs d
