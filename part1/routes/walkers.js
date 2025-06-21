4d9fc62d55769828345e56ec66b48df8d87c8f4c
henryn3005 <ineverdie69@hotmail.com>
2025-06-21 03:49:45 +0000

    [automatic commit] 2025-06-21 03:49:45 UTC 14.2.84.218

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -23,7 +23,7 @@ router.get('/api/walers/summary', async (req, res) => {
         `);
         res.json(rows);
     } catch (err) {
-        res.status(500).
+        res.status(500).json({ error: 'failed to return'});
     }
 
 });
