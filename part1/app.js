43a758a87c705b4de87061d9e3de413b64b7c17d
henryn3005 <ineverdie69@hotmail.com>
2025-06-21 04:24:20 +0000

    [automatic commit] 2025-06-21 04:24:20 UTC 14.2.84.218

--- part1/app.js
+++ part1/app.js
@@ -141,6 +141,18 @@ let db;
         (1, '2025-07-10 09:00:00', 30, 'Mars', 'open'),
       `);
     }
+
+    const [ratingsRows] = await db.execute('SELECT COUNT(*) AS count FROM WalkRequests');
+    if (walkrequestsRows[0].count === 0) {
+      await db.execute(`
+        INSERT INTO WalkRequests (dog_id, request_time, duration_minutes, location, status) VALUES
+        (1, '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+        (1, '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+        (1, '2025-06-10 10:00:00', 20, 'Royal Lane', 'accepted'),
+        (1, '2025-06-10 04:35:00', 60, 'Royal Lane', 'open'),
+        (1, '2025-07-10 09:00:00', 30, 'Mars', 'open'),
+      `);
+    }
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
   }
